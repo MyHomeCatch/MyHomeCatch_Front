4526a492b4bbc4d6f0a5dc6498a81a53ac4efe53
JH Kim <rlawogus65@gmail.com>
2025-07-18 15:28:12 +0900

    fix: tailwind css 플러그인 제거

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-   server: {
-	 proxy: {
-		 '/api': {
-			 target: 'http://localhost:3000', // json-server
-			 changeOrigin: true,
-			 rewrite: (path) => path.replace(/^\/api/, ''),
-		 },
-		},
-	 },
-})
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // json-server
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
