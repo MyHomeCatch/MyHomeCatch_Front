1a4004da48475c3e050dd45b2ddd0306243794c1
ahranah <window765@khu.ac.kr>
2025-07-29 18:28:52 +0900

    feat: img section

---
+++ src/pages/DetailPage/DetailPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="detail-page">
+    <!-- 제목 -->
+
+    <!-- 상단 이미지 섹션 -->
+    <ImageSection />
+
+    <div class="main-content">
+      <!-- 좌측 지도 영역 -->
+
+      <!-- 우측 바 -->
+
+    </div>
+
+    <!-- 하단 댓글/게시판 -->
+
+  </div>
+</template>
+
+<script setup>
+import ImageSection from '@/components/DetailPage/ImageSection.vue'
+
+</script>
+
+<style scoped>
+
+</style>
